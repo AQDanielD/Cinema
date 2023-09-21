588a0e3a586c8b4a67f02763667dd8f270903d9f
Daniel Davies <AQ232596@aquinas.ac.uk>
2023-09-21 13:41:10 +0100

    a

--- Program.cs
+++ Program.cs
@@ -5,6 +5,35 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Cinema
+
+
+    /*Class for structure of movies:
+     * 
+     * 
+     * Link code to database for getting information of movies
+     * 
+     * 
+     * 
+     * 
+     * Group paying
+     * Ask for amount of adults and children
+     * confirm the age ranges
+     * ask for all teh seats
+     * ask if they have any disabilities (front row seats)
+     * 
+     * 
+     * 
+     * 2D array for seats
+     * 1 for taken
+     * 0 for empty
+     *
+     * 
+     * Start While loop -> Ask if they want to continue as guest or login -> check if they are admin or just a user by the Database in table users
+     * Show the current available movies with their ratings availability -> ask if its a group or not 
+     * Group-> get group size -> for for disabilities and age range
+     *                         Acceptable age
+     *                              Yes -> Disabilities -> show times whithin the age range which are suita
+     */ 
 {
     internal class Program
     {
